09f15f69317c16795ab0e21b79a893f44261c8ac
[yuushin-prac] <[yuushin.n999n@gmail.com]>
2023-03-27 11:06:19 +0900

    set CRUD API

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'site#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    resources :events, only: %i[index show create update destroy]
+  end
 end
